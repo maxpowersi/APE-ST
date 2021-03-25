8790d896edd51ed7c05c4d02e397a2dd484fc00e
Maxpowersi <maxi1990@gmail.com>
2021-03-25 20:30:13 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,53 +1,51 @@
 mkdir ape-tools
 cd ape-tools
+sudo apt-get install smtp-user-enum
+sudo apt-get install sslscan
 sudo apt-get install xsltproc
 sudo apt-get install nmap
 sudo apt-get install ncrack
-sudo apt-get install smtp-user-enum
-sudo apt-get install sslscan
 sudo apt-get install nikto
+sudo apt-get install gobuster
 pip install validators
 pip3 install webscreenshot
 pip3 install dirhunt
 pip3 install festin
 npm install -g retire
-npm -g install js-beautify
-npm i -g wappalyzer
-go get github.com/OJ/gobuster
+npm install -g js-beautify
+npm install -g -unsafe-permwappalyzer
 go get -u github.com/tomnomnom/httprobe
 go get github.com/003random/getJS
 go get github.com/tomnomnom/waybackurls
 go get -u github.com/tomnomnom/assetfinder
 go get github.com/hakluke/hakrawler
-go get github.com/tismayil/ohmybackup.git
-go get github.com/tismayil/fockcache.git
-wget https://github.com/OWASP/Amass/releases/download/v3.2.0/amass_v3.2.0_linux_amd64.zip
+go get github.com/tismayil/ohmybackup
+go get github.com/tismayil/fockcache
+go get https://github.com/michenriksen/gitrob
+go get github.com/haccer/subjack
+sudo snap install amass
 wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2 
 wget https://github.com/michenriksen/aquatone/releases/download/v1.7.0/aquatone_linux_amd64_1.7.0.zip
 #TODO: Extract the  zip, and add to the path the tools
 wget https://testssl.sh/testssl.sh
 git clone https://github.com/ernw/nmap-parse-output.git
 git clone https://github.com/codingo/Interlace.git
-git clone https://github.com/ernw/nmap-parse-output.git
 git clone https://github.com/aboul3la/Sublist3r.git
 git clone https://github.com/tomdev/teh_s3_bucketeers.git
 git clone https://github.com/guelfoweb/knock.git
-git clone https://github.com/michenriksen/gitrob.git
 git clone https://github.com/stanislav-web/OpenDoor.git
 git clone https://github.com/GerbenJavado/LinkFinder.git
 git clone https://github.com/s0md3v/Photon.git
 git clone https://github.com/jobertabma/virtual-host-discovery.git
 git clone https://github.com/ZephrFish/GoogD0rker.git
 git clone https://github.com/1N3/Goohak.git
-git clone https://github.com/haccer/subjack.git
 git clone https://github.com/Rhynorater/CVE-2018-15473-Exploit.git
 git clone https://github.com/arthepsy/ssh-audit.git
 git clone https://github.com/BountyStrike/Injectus.git
 git clone https://github.com/In3tinct/See-SURF.git
 git clone https://github.com/Nekmo/dirhunt.git
-git clone https://github.com/hakluke/hakrawler.git
 git clone https://github.com/bl4ckmamb4/OpenRelayMagic.git
 git clone https://github.com/s0md3v/XSStrike.git
-git clone https://github.com/securing/DumpsterDiver
-git clone https://github.com/daylen/api-key-detect
+git clone https://github.com/securing/DumpsterDiver.git
+git clone https://github.com/daylen/api-key-detect.git
 #TODO; go to repo and install the tool, and add to the path
